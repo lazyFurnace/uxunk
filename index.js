2bc4679b00ff02e89335a25cf1b9d0cf801b9417
qi.li <787139384@qq.com>
2018-06-24 18:00:10 +0800

    预备开始

---
+++ index.js
@@ -0,0 +1,57 @@
+// 观察者模式
+function createObserver(reducer, defaultState) {
+
+    let state = defaultState;
+    let eventQueue = {};
+
+    function dispatch(type, action) {
+        state = reducer(state, type, action);
+        this.listen(type);
+    }
+    function getState() {
+        return JSON.parse(JSON.stringify(state));
+    }
+    function listen(name, fn) {
+        if(!fn) return;
+        if(!eventQueue[name]) {
+            eventQueue[name] = [fn];
+        } else if(eventQueue[name] && !eventQueue[name].some(item => item === fn)) {
+            eventQueue[name].push(fn);
+        } else {
+            console.log('This function has been subscribed!');
+        }
+        return function unlisten() {
+            if(!eventQueue[name]) return;
+
+            const index = eventQueue[name].indexOf(fn);
+            eventQueue[name].splice(index, 1);
+
+            if(eventQueue[name].length === 0) {
+                delete eventQueue[name];
+            }
+        }
+    }
+    function trigger(name, action) {
+        if(eventQueue[name]) {
+            eventQueue[name].forEach((item) => {
+                item(action);
+            })
+        } else {
+            console.log('No related subscriptions!');
+        }
+    }
+    function remove(name) {
+        if(eventQueue[name]) {
+            delete eventQueue[name];
+        } else {
+            console.log('No related subscriptions!');
+        }
+    }
+    return {
+        listen: listen,
+        trigger: trigger,
+        remove: remove,
+        dispatch: dispatch,
+        getState: getState
+    }
+}
