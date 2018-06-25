968568d1de5fb8f9f17d8ece3e7774ba444c0423
qi.li <787139384@qq.com>
2018-06-26 00:49:33 +0800

    修理了所有eslint报错

--- src/index.js
+++ src/index.js
@@ -6,7 +6,6 @@
  * defaultState 是默认的数据
  */
 function uxunk(reducer, defaultState) {
-
     // state 中存放所有数据
     let state = defaultState;
 
@@ -17,10 +16,10 @@ function uxunk(reducer, defaultState) {
      *  ...
      * }
      */
-    let eventQueue = {};
+    const eventQueue = {};
 
-    /* 
-     * 用于处理传入的 type 和 action 
+    /*
+     * 用于处理传入的 type 和 action
      * type 为所触发订阅的类型 action 为修改数据所需要的参数，是一个对象 必须有type属性
      * dispatch 函数作用是将 当前数据、订阅类型、修改数据的 action 传入 reducer 中
      * reducer 返回修改后的所有数据 并触发当前订阅类型下发布给所有订阅的函数
@@ -30,56 +29,58 @@ function uxunk(reducer, defaultState) {
         this.trigger(type);
     }
 
-    //获取当前所有数据
+    // 获取当前所有数据
     function getState() {
         return JSON.parse(JSON.stringify(state));
     }
 
-    //绑定订阅类别、监听函数
-    //返回函数用于接触订阅
+    // 绑定订阅类别、监听函数
+    // 返回函数用于接触订阅
     function listen(name, fn) {
-        if(!fn) return;
-        if(!eventQueue[name]) {
+        if (!fn) return false;
+        if (!eventQueue[name]) {
             eventQueue[name] = [fn];
-        } else if(eventQueue[name] && !eventQueue[name].some(item => item === fn)) {
+        } else if (eventQueue[name] && !eventQueue[name].some(item => item === fn)) {
             eventQueue[name].push(fn);
         } else {
-            console.log('This function has been subscribed!');
+            throw Error('This function has been subscribed!');
         }
         return function unlisten() {
-            if(!eventQueue[name]) return;
+            if (!eventQueue[name]) return;
 
             const index = eventQueue[name].indexOf(fn);
             eventQueue[name].splice(index, 1);
 
-            if(eventQueue[name].length === 0) {
+            if (eventQueue[name].length === 0) {
                 delete eventQueue[name];
             }
-        }
+        };
     }
-    //触发订阅类型的所有订阅了的函数
+    // 触发订阅类型的所有订阅了的函数
     function trigger(name, action) {
-        if(eventQueue[name]) {
+        if (eventQueue[name]) {
             eventQueue[name].forEach((item) => {
                 item(action);
-            })
+            });
         } else {
-            console.log('No related subscriptions!');
+            throw Error('No related subscriptions!');
         }
     }
-    //删除该订阅类型下的所有函数
+    // 删除该订阅类型下的所有函数
     function remove(name) {
-        if(eventQueue[name]) {
+        if (eventQueue[name]) {
             delete eventQueue[name];
         } else {
-            console.log('No related subscriptions!');
+            throw Error('No related subscriptions!');
         }
     }
     return {
-        listen: listen,
-        trigger: trigger,
-        remove: remove,
-        dispatch: dispatch,
-        getState: getState
-    }
+        listen,
+        trigger,
+        remove,
+        dispatch,
+        getState
+    };
 }
+
+export default uxunk;
---
+++ src/uxunk.js
@@ -0,0 +1,127 @@
+import uxunk from './index';
+
+// actionType.js
+const actionAddition = 'Addition';
+const actionSubtraction = 'Subtraction';
+
+// action.js
+const forceHandling = (type, name) => ({ type, name });
+
+// reducer.js
+const reducer = (state, type, action) => {
+    switch (action.type) {
+    case actionAddition:
+        return state.map((item) => {
+            let { troops } = item;
+            if (item.name === action.name) {
+                troops += 1;
+            }
+            return Object.assign({}, item, { troops });
+        });
+    case actionSubtraction:
+        return state.map((item) => {
+            let { troops } = item;
+            if (item.name === action.name) {
+                troops -= 1;
+            }
+            return Object.assign({}, item, { troops });
+        });
+    default:
+        return state;
+    }
+};
+
+// 原始数据
+const defaultState = [
+    {
+        name: '帝国',
+        troops: 0
+    }, {
+        name: '矮人',
+        troops: 0
+    }, {
+        name: '高等精灵',
+        troops: 0
+    }, {
+        name: '兽人',
+        troops: 0
+    }
+];
+
+// 绑定store
+const store = uxunk(reducer, defaultState);
+
+// 修改所触发函数
+const getData = () => {
+    const storeData = window.store.getState();
+    let troopsAll = 0;
+    storeData.forEach((item, index) => {
+        document.getElementsByClassName('troops-num')[index].innerHTML = item.troops;
+        troopsAll += item.troops;
+    });
+    document.getElementsByClassName('troops-all')[0].innerHTML = troopsAll;
+};
+
+// 监听 getData 函数
+store.listen('changeTroops', getData);
+
+// elem
+const root = document.getElementById('root');
+
+// 根据原始数据初次渲染页面
+(function render(state) {
+    const flag = document.createDocumentFragment();
+    const ul = document.createElement('ul');
+
+    state.forEach((item) => {
+        const li = document.createElement('li');
+
+        const b = document.createElement('b');
+        b.innerHTML = `${item.name}：`;
+
+        const p = document.createElement('p');
+
+        const add = document.createElement('button', { type: 'button' });
+        add.type = 'submit';
+        add.innerHTML = '+';
+        add.dataset.key = actionAddition;
+        add.dataset.index = item.name;
+
+        const sub = document.createElement('button', { type: 'button' });
+        sub.type = 'button';
+        sub.innerHTML = '-';
+        sub.dataset.key = actionSubtraction;
+        sub.dataset.index = item.name;
+
+        const span = document.createElement('span');
+        span.className = 'troops-num';
+        span.innerHTML = item.troops;
+
+        p.appendChild(add);
+        p.appendChild(span);
+        p.appendChild(sub);
+
+        li.appendChild(b);
+        li.appendChild(p);
+
+
+        ul.appendChild(li);
+    });
+
+    const div = document.createElement('div');
+    div.innerHTML = '<b>一共：</b><span class="troops-all">0</span>';
+
+    flag.appendChild(ul);
+    flag.appendChild(div);
+
+    root.appendChild(flag);
+}(defaultState));
+
+// 绑定监听事件
+root.addEventListener('click', (e) => {
+    if (e.target.nodeName !== 'BUTTON') {
+        return;
+    }
+    const { target } = e;
+    store.dispatch('changeTroops', forceHandling(target.dataset.key, target.dataset.index));
+});
