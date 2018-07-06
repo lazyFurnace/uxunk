e1bdd4a7558ecc81eb67588e5069761dbda13e77
李齐 <787139384@qq.com>
2018-07-06 18:34:09 +0800

    Update index.js

--- src/index.js
+++ src/index.js
@@ -5,3 +5,30 @@ export {
     combineReducers,
     createStore
 };
+
+
+
+function bindActionCreators(actionCreators, dispatch) {
+    let boundActionCreators = {};
+    Object.keys(actionCreators).forEach(item => {
+        let actionCreator = actionCreators[item];
+        boundActionCreators[item] = (
+            (actionCreator, dispatch) => (...arg) => dispatch(actionCreator(arg))
+        )(actionCreator, dispatch)
+    })
+    return boundActionCreators;
+}
+
+function a(data) {
+    return data;
+}
+function b(data) {
+    return data;
+}
+function dispatch(data) {
+    console.log(data);
+}
+
+var actionCreators = {a,b};
+
+var admin = bindActionCreators(actionCreators, dispatch)
