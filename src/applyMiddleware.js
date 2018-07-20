38f7e71d3c83e6ad499bf10f39f8d4077c507ade
李齐 <787139384@qq.com>
2018-07-20 18:38:05 +0800

    Update applyMiddleware.js

--- src/applyMiddleware.js
+++ src/applyMiddleware.js
@@ -1,3 +1,41 @@
 function applyMiddleware(...middle) {
   
 }
+
+
+function compose(...funs) {
+    return funs.reduce((a, b) => {
+        return (...arg) => {
+            return a(b(...arg))
+        }
+    })
+}
+  
+function thunk() {
+    return (next) => action => {
+
+        console.log('这是 thunk 函数');
+    
+        return next(action);
+    };
+}
+
+function logger() {
+    return (next) => (action) => {
+		console.log(next)
+        console.log('这是 logger 函数')
+
+        return next(action)
+    }
+}
+var middlewares = [thunk , logger];
+var chain = middlewares.map(middleware => middleware());
+
+var dispatch = (data) => {
+    console.log(data)
+    return data;
+}
+
+var admin = compose(...chain)(dispatch)
+
+admin('wodetian')
