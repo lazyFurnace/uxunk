d793c78b85773cd2c65ea888d7629369f1df8ed7
qi.li <787139384@qq.com>
2018-07-25 23:50:51 +0800

    add

--- src/applyMiddleware.js
+++ src/applyMiddleware.js
@@ -9,9 +9,22 @@
  * 在使用这个函数的情况下 store 的创建将在这个函数中进行
  */
 function applyMiddleware(...middlewares) {
+    /**
+     * 配合 createStore 中的 enhancer 来实现有中间件的 store 的创建
+     * 有中间件的 store 会在触发 dispatch 后，执行 reducer 前执行所有的中间件
+     * applyMiddleware 返回的是一个柯里化的函数
+     * 第一次接收 redux 的 createStore
+     * 第二次接收创建 store 所需要的 reducer 和 preloadedState
+     * 两次接收后创建 store
+     */
     return (createStore) => (...arg) => {
         const store = createStore(...arg);
-
+        /**
+         * 写一个空的 dispatch 函数，这个 dispatch 将是用来链式触发中间件的 dispatch 方法
+         * 这个 dispatch 不是真正 store 上的 dispatch，而是触发所有中间件的 dispatch
+         * 声明 middlewareAPI 里面是所有中间件都需要用到的 getState 和 dispatch 方法
+         * 在中间件中调用这里的 dispatch 方法将会重新走一遍所有中间件
+         */
         let dispatch = () => {};
 
         const middlewareAPI = {
