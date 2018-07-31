001bb6254e8bb801558f00f1ccd0741768374a10
qi.li <787139384@qq.com>
2018-07-31 22:32:46 +0800

    重写 createStore

--- src/createStore.js
+++ src/createStore.js
@@ -3,85 +3,78 @@
  * 按照 redux 源码进行仿制
  * 发布/订阅模式
  * 拥有 redux 的几乎所有功能
- * @param {Function} reducer 用于存放所有的数据处理逻辑
+ * @param {Function} reducer 用于存放所有的数据处理逻辑，返回下一个state树
  * 
  * @param {Object} defaultState 默认的初始化 state
  * 
- * @param {Function} enhancer 为 redux 提供所有中间件
+ * @param {Function} enhancer 为 redux 提供所有中间件，只能使用'applyMiddleware'方法来生成
  * 
  * @return {Object} 返回 store 里面包含 redux 所有数据及方法
  */
 export default function createStore(reducer, defaultState, enhancer) {
-    // state 中存放所有数据
-    let state = defaultState;
+    // 判断是不是没有 defaultState 只有 enhancer 如果是这样就交换一下
+    if(typeof enhancer === 'undefined' && typeof defaultState === 'function') {
+        enhancer = defaultState;
+        defaultState = undefined;
+    }
+    // 如果有中间件就在中间件中执行 createStore
+    if(typeof enhancer === 'function') {
+        return enhancer(createStore)(reducer, defaultState);
+    }
+    
+    let currentState = defaultState;
     let currentReducer = reducer;
+    let currentListeners = [];
 
-    /*
-     * eventQueue 存放所有发布\订阅的函数
-     * eventQueue = {
-     *  type: [fn, fn, fn, ...],
-     *  ...
-     * }
+    /**
+     * dispatch 函数，执行 reducer ，触发所有 listener
+     * 
+     * @param {Object} action 触发发布/订阅的事件
+     * 
+     * @return {Object} 执行后返回 action
      */
-    const eventQueue = {};
-
-    /*
-     * 用于处理传入的 type 和 action
-     * type 为所触发订阅的类型 action 为修改数据所需要的参数，是一个对象 必须有type属性
-     * dispatch 函数作用是将 当前数据、订阅类型、修改数据的 action 传入 reducer 中
-     * reducer 返回修改后的所有数据 并触发当前订阅类型下发布给所有订阅的函数
-     */
-    function dispatch(type, action) {
-        state = currentReducer(state, type, action);
-        this.trigger(type);
+    function dispatch(action) {
+        currentState = currentReducer(currentState, action);
+        currentListeners.forEach(item => item());
+        return action;
     }
 
-    // 获取当前所有数据
+    /**
+     * getState 函数，返回经过深克隆的 state 树
+     */
     function getState() {
-        return JSON.parse(JSON.stringify(state));
+        return JSON.parse(JSON.stringify(currentState));
     }
 
-    // 绑定订阅类别、监听函数
-    // 返回函数用于接触订阅
-    function subscribe(name, fn) {
-        if (!fn) return false;
-        if (!eventQueue[name]) {
-            eventQueue[name] = [fn];
-        } else if (eventQueue[name] && !eventQueue[name].some(item => item === fn)) {
-            eventQueue[name].push(fn);
-        } else {
+    /**
+     * subscribe 函数，用于绑定 触发 dispatch 更新 state 时触发的函数
+     * 
+     * @param {Function} fn 传入需要加入 listeners 绑定的函数
+     * 
+     * @return {Function} 解除改函数绑定的方法
+     */
+    function subscribe(fn) {
+        // 如果 fn 没有或不是一个 function 抛出错误
+        if(!fn || typeof fn !== 'function') {
             throw Error('This function has been subscribed!');
         }
+        // listeners 里没有这个时，加进去
+        if(currentListeners.indexOf(fn) < 0) {
+            currentListeners.push(fn);
+        }
+        // 返回解除 listeners 绑定的方法
         return function unsubscribe() {
-            if (!eventQueue[name]) return;
-
-            const index = eventQueue[name].indexOf(fn);
-            eventQueue[name].splice(index, 1);
-
-            if (eventQueue[name].length === 0) {
-                delete eventQueue[name];
+            const index = currentListeners.indexOf(fn);
+            if(index > 0) {
+                currentListeners.splice(index, 1);
             }
         };
     }
-    // 触发订阅类型的所有订阅了的函数
-    function trigger(name, action) {
-        if (eventQueue[name]) {
-            eventQueue[name].forEach((item) => {
-                item(action);
-            });
-        } else {
-            throw Error('No related subscriptions!');
-        }
-    }
-    // 删除该订阅类型下的所有函数
-    function remove(name) {
-        if (eventQueue[name]) {
-            delete eventQueue[name];
-        } else {
-            throw Error('No related subscriptions!');
-        }
-    }
-    // 替换reducer
+
+    /**
+     * replaceReducer 函数，接收一个新的 reducer 代替旧的
+     * @param {*} newReducer 
+     */
     function replaceReducer(newReducer) {
         currentReducer = newReducer;
     }
@@ -89,8 +82,6 @@ export default function createStore(reducer, defaultState, enhancer) {
         subscribe,
         dispatch,
         getState,
-        replaceReducer,
-        trigger,
-        remove
+        replaceReducer
     };
 }
