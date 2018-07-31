9033db28404064a5ce23f628bdfca0e097379dd3
qi.li <787139384@qq.com>
2018-07-31 23:57:51 +0800

    添加 cross-env 支持各种环境下的指令 拆分指令 uxunk 打包 和 例子打包 根据 process.env.NODE_ENV 判断打什么包 例子打包后放在 component 文件夹中 例子打包前放在 example 文件夹中

---
+++ example/index.js
@@ -0,0 +1,3 @@
+import * as uxunk from '../src/index';
+
+console.log(uxunk);
--- rollup.config.js
+++ rollup.config.js
@@ -1,9 +1,33 @@
-export default {
-    input: './src/index.js',
-    output: {
-        file: 'uxunk.js',
-        format: 'umd',
-        name: 'uxunk',
-        sourcemap: 'inline'
-    }
-  }
+const ENV = process.env.NODE_ENV;
+let rollupModule = {};
+switch (ENV) {
+    case 'library':
+        rollupModule = {
+            // 入口
+            input: './src/index.js',
+            // 出口
+            output: {
+                // 路径
+                file: 'uxunk.js',
+                // 生成包的格式
+                format: 'umd',
+                // 生成包名称(浏览器中导入 window 的名字)
+                name: 'uxunk',
+                sourcemap: 'inline'
+            }
+        }
+        break;
+    case 'example':
+        rollupModule = {
+            input: './example/index.js',
+            output: {
+                file: './component/index.js',
+                format: 'umd',
+                sourcemap: 'inline'
+            }
+        }
+        break;
+    default: 
+        throw Error('未设置环境变量');
+}
+export default rollupModule;
